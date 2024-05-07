acaf5a41bbbb4f8bd745ee5a750c8f8ed5f4a9cd
Jacob Ye <gmding777@gmail.com>
2024-05-07 06:15:30 -0700

    Added battlehandler, added some fields to card

---
+++ react-app/src/game/battlehandler.js
@@ -0,0 +1,23 @@
+export default function Battlehandler(allies, opponents) {
+    const party = allies;
+    const monsters = opponents;
+    const battledeck = [];
+
+    // Load each card from each ally into the deck.
+    party.forEach((ally) => {
+        let deck = ally.getDeck().getCards()
+        for (let i = 0; i < deck.length; i++) {
+            battledeck.push(ally.getName(), deck[i]);
+        }
+    })
+
+    const getAllies = () => party;
+    const getOpponents = () => monsters;
+
+    /*
+    get target from some sort of targetting system handled by the game.
+    
+    */
+
+    return { getAllies, getOpponents }
+}
--- react-app/src/game/card.js
+++ react-app/src/game/card.js
@@ -1,3 +1,11 @@
-export default function Card() {
+export default function Card(name, data) {
+    const cardName = name;
+    const desc = data.desc;
 
+    // more functions to be addedd later when the battle loop is figured out.
+
+
+    const getCardName = () => cardName;
+
+    return { getCardName }
 }
